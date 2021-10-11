2a244036a801bcdb7003e701565a2981967691f8
yangxuan@enjoymi.com <yangxuan@enjoymi.com>
2021-10-11 11:22:14 +0800

    忽略配置文件上传

---
+++ config/wxKeysConfig.js
@@ -0,0 +1,7 @@
+module.exports = {
+    appid: "wx4ee88cd48b98fc02",// 小程序的appid
+    appsecret: "61803b757df764b876cc3e8c95c0df92",// 小程序的appSecret
+    mch_id: "1614817099", // 微信商户号
+    mchkey: "YZY727196960116cywyrljl888888888",  // 微信商户的key 32位
+    notify_url: "http://127.0.0.19:8087/wxpayresult" //通知地址
+}
