e70dadf3e3f9d02e8c2340d29a2f4137beca4f73
yangxuan <1130180692@qq.com>
2022-03-29 03:59:24 +0800

    增加上传接口

--- src/routers/txCos.js
+++ src/routers/txCos.js
@@ -2,6 +2,7 @@ var express = require('express')
 var fs = require('fs');
 var router = express.Router();
 var config = require('../../config/txCos.json')
+var https = require('https');
 var COS = require('cos-nodejs-sdk-v5');
 var multer = require("multer");
 var multerMiddleware = multer()  //实例化multer
@@ -17,7 +18,6 @@ router.post('/search', async (req, res, next) => {
             Region: 'ap-beijing',     /* 必须 */
             // Prefix: req.path,         /* 非必须 */
         }, function (err, data) {
-            console.log(err || data.Contents);
             if (err) {
                 res.send({
                     data: err,
@@ -59,5 +59,46 @@ router.post('/upload', multerMiddleware.single('file'), async (req, res, next) =
         next(error)
     }
 })
+
+router.post('/saveAvatar',function (req1, res1, next) {
+    try {
+        let url = req1.body.filePath
+        var req = https.get(url, function (res) {
+            var imgData = "";
+            res.setEncoding("binary"); //一定要设置response的编码为binary否则会下载下来的图片打不开
+            res.on("data", function (chunk) {
+                imgData += chunk;
+            });
+            res.on("end",async function () {
+                await cos.putObject({
+                    Bucket: 'runners-1307290574', /* 填入您自己的存储桶，必须字段 */
+                    Region: 'ap-beijing',  /* 存储桶所在地域，例如ap-beijing，必须字段 */
+                    Key: req1.body.folder + req1.body.fileName,
+                    Body: Buffer.from(imgData, "binary"), /* 必须 */
+                }, function (err, data) {
+                    if (err) {
+                        res1.send({
+                            data: err,
+                            message: '成功！'
+                        });
+                    } else {
+                        res1.send({
+                            data: data,
+                            message: '成功！'
+                        });
+                    }
+                });
+            });
+            res.on("error", function (err) {
+                console.log("请求失败");
+            });
+        });
+        req.on('error', function (err) {
+            console.log("请求失败2" + err.message);
+        });
+    } catch (error) {
+        next(error)
+    }
+});
 
 module.exports = router
